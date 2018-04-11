a4528d23e987aff67db8150abdae2bfa2794aa21
Martin Dvoracek <Dworza@gmail.com>
2018-04-11 12:00:04 +0200

    Update TimeController.java

--- src/main/java/com/javaproject/timeprovider/controller/TimeController.java
+++ src/main/java/com/javaproject/timeprovider/controller/TimeController.java
@@ -12,6 +12,6 @@ public class TimeController {
 
     @RequestMapping(method = RequestMethod.GET)
     String getTime() {
-        return (String.valueOf(LocalTime.now()));
+        return String.valueOf(LocalTime.now());
     }
 }
