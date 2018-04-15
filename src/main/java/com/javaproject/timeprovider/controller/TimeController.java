3ecfb1270aa150b988353a093fdf5638f04c487f
madv <martin.dvoracek@mimacom.com>
2018-04-16 01:18:34 +0200

    logging added

--- src/main/java/com/javaproject/timeprovider/controller/TimeController.java
+++ src/main/java/com/javaproject/timeprovider/controller/TimeController.java
@@ -1,5 +1,6 @@
 package com.javaproject.timeprovider.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,12 @@ import java.time.LocalTime;
 @RequestMapping(value = "/")
 public class TimeController {
 
+
+    private static final Logger logger = Logger.getLogger(TimeController.class);
+
     @RequestMapping(method = RequestMethod.GET)
     String getTime() {
+        logger.info("WOHOOO! SOMEBODY CALLED ME!");
         return ("The exact time on pod " + System.getenv("HOSTNAME") + " is: " + String.valueOf(LocalTime.now()));
     }
 }
