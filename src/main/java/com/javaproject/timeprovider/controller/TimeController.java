cf58d7c00a4ccec64a5060ba3fd9d01118aa6d9b
madv <martin.dvoracek@mimacom.com>
2018-04-16 02:04:12 +0200

    actuator added

--- src/main/java/com/javaproject/timeprovider/controller/TimeController.java
+++ src/main/java/com/javaproject/timeprovider/controller/TimeController.java
@@ -17,6 +17,6 @@ public class TimeController {
     @RequestMapping(method = RequestMethod.GET)
     String getTime() {
         logger.info("WOHOOO! SOMEBODY CALLED ME!");
-        return ("The time the on pod " + System.getenv("HOSTNAME") + " is: " + String.valueOf(LocalTime.now()));
+        return ("The time on the pod " + System.getenv("HOSTNAME") + " is: " + String.valueOf(LocalTime.now()));
     }
 }
